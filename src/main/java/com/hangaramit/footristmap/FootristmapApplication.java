fb57a63eb444e46f2238025862297f55b60a2eb1
hsyoodev <hsyoodev@gmail.com>
2023-11-23 14:34:34 +0900

    feat: set up foodristmap project

---
+++ src/main/java/com/hangaramit/footristmap/FootristmapApplication.java
@@ -0,0 +1,13 @@
+package com.hangaramit.footristmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootristmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootristmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hangaramit/footristmap/FootristmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangaramit.footristmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootristmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
