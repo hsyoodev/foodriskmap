dd9a8ced6605570f5ad71d62c4dfc445bbdd22c6
waraukuma <waraukuma94@gmail.com>
2023-11-27 10:16:12 +0900

    feat: Add login and Edit project files

--- src/main/java/com/hangaramit/foodriskmap/controller/MemberController.java
+++ src/main/java/com/hangaramit/foodriskmap/controller/MemberController.java
@@ -1,5 +1,52 @@
 package com.hangaramit.foodriskmap.controller;
 
+import com.hangaramit.foodriskmap.config.SecurityConfig;
+import com.hangaramit.foodriskmap.entity.Member;
+import com.hangaramit.foodriskmap.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
 public class MemberController {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @GetMapping("member/signin")
+    public String signin() {
+        return "member/signin";
+    }
+
+    @Autowired
+    SecurityConfig securityConfig;
+
+    @PostMapping("/member/signin")
+    public String memberLogin(@ModelAttribute Member member) {
+        Member joinMember = memberRepository.findByEmail(member.getEmail());
+        if (joinMember != null) {
+            String joinPwd = member.getPwd();
+            String memberPwd = member.getPwd();
+            boolean isMatch = passwordEncoder.matches(memberPwd, joinPwd);
+            session.setAtrribute("member_info", member);
+        }
+        return "redirect:/";
+    }
+
+//    @PostMapping("/join")
+//    {
+    //회원 가입
+//    }
+
+
+//    //회원 로그인은 가입 후 확인가능 잠시보류
+//    @PostMapping("/member/login")
+//        public String memberLogin(@RequestParam String email, String pwd, Model model) {
+//        model.addAttribute(email, pwd);
+//        return "";
+//    }
+
 
 }
--- src/main/java/com/hangaramit/foodriskmap/repository/MemberRepository.java
+++ src/main/java/com/hangaramit/foodriskmap/repository/MemberRepository.java
@@ -1,5 +1,16 @@
 package com.hangaramit.foodriskmap.repository;
 
-public interface MemberRepository {
+import com.hangaramit.foodriskmap.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    public Member findByEmailAndPwd(String email, String pwd);
+
+    //사용자 정의 메소드 //이메일이 일치
+    public Member findByEmail(String email);
+
 
 }
